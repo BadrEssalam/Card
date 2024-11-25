7b456ece9a614b7cd78790f2e0bca9cc1c700087
BadrEssalam <badressalambdrilling@gmail.com>
2024-11-25 23:53:56 +0100

    modifying

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CardAsContainer(),
+      home: CardWidget(),
     );
   }
 }
 
-class CardAsContainer extends StatelessWidget{
-
-  const CardAsContainer({super.key});
+class CardWidget extends StatelessWidget {
+  const CardWidget({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar : AppBar(),
-          drawer: const Drawer(),
-          body: Card(
-            elevation: 20,
-            shadowColor: Colors.blue,
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
-                side: BorderSide(
-                    color: Colors.white70,
-                    width: 5
-                ) ),//Border.all(color: Colors.black12, width: 5, style: BorderStyle.solid, strokeAlign: BorderSide.strokeAlignOutside),
-            color: Colors.redAccent,
-            child: Container(margin: EdgeInsets.all(10), child: const Text("Working by willing of god, build the success",textAlign: TextAlign.center, style: TextStyle(fontSize: 50, ))),
-          ),
-        )
-    );
+      appBar: AppBar(),
+      drawer: const Drawer(),
+      body: Card(
+        elevation: 20,
+        shadowColor: Colors.blue,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+            side: BorderSide(color: Colors.white70, width: 5)),
+        color: Colors.redAccent,
+        child: Container(
+            margin: const EdgeInsets.all(10),
+            child: const Text("Working by willing of god, build the success",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 50,
+                ))),
+      ),
+    ));
   }
 }
