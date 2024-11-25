f2c013daa38c018a815a46d1f05318db433ae583
BadrEssalam <badressalambdrilling@gmail.com>
2024-11-25 23:21:40 +0100

    make the Text widget contained inside a Container

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,17 @@ class CardAsContainer extends StatelessWidget{
         home: Scaffold(
           appBar : AppBar(),
           drawer: const Drawer(),
-          body: const Card(
-            elevation: 100,
+          body: Card(
+            elevation: 20,
             shadowColor: Colors.blue,
-            shape:   RoundedRectangleBorder(
+            shape: const RoundedRectangleBorder(
                 borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
                 side: BorderSide(
                     color: Colors.white70,
                     width: 5
                 ) ),//Border.all(color: Colors.black12, width: 5, style: BorderStyle.solid, strokeAlign: BorderSide.strokeAlignOutside),
             color: Colors.redAccent,
-            child: Text("Please, enter your data", style: TextStyle(fontSize: 50)),
+            child: Container(margin: EdgeInsets.all(10), child: const Text("Working by willing of god, build the success",textAlign: TextAlign.center, style: TextStyle(fontSize: 50, ))),
           ),
         )
     );
